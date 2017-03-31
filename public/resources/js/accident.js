465a7f9c501b87c3333ac63fd5deac4c6c2c73fc
jyoon2 <jyoon2@nd.edu>
2017-03-31 14:34:26 -0400

    added logout confirm to each page

--- public/resources/js/accident.js
+++ public/resources/js/accident.js
@@ -4,9 +4,3 @@ function submitButton(){
   window.location.href = "print_and_email.html";
   console.log("Submit Button Clicked.")
 }
-
-function myFunction() {
-  if (confirm("Are you sure you want to logout?") == true) {
-    window.location.href = "index.html";
-  }
-}
