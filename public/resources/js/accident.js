eee96edb887ace9cc45a1b4cd9fd47b120088846
jyoon2 <jyoon2@nd.edu>
2017-04-04 02:44:26 -0400

    sample pushing to firebase

--- public/resources/js/accident.js
+++ public/resources/js/accident.js
@@ -1,4 +1,11 @@
 
+function writeAccidentData() {
+  firebase.database().ref('accident/').push({
+    name: "Child Name",
+    staff: "Staff Name",
+    description: "Description of Event"
+  });
+}
 
 function submitButton(){
   window.location.href = "print_and_email.html";
