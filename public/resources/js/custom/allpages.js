ff09a7b04f46b986ef69362c7c39ec8939c59a4b
jyoon2 <jyoon2@nd.edu>
2017-04-27 14:47:27 -0400

    testing pdfmake, works from print_and_email with dummy values

--- public/resources/js/custom/allpages.js
+++ public/resources/js/custom/allpages.js
@@ -33,5 +33,12 @@ function backToChoose() {
 
 //Creates a pdf and prints from json
 function printForm(doc_def) {
+  var dd = {
+	content: [
+		'First paragraph',
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+	]
+}
+  pdfMake.createPdf(dd).open();
     //var pdfDoc = printer.creadtePdf
 }
