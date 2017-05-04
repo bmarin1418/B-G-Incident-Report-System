7bdc90521399b021df32058797e5177118a08e97
bmarin1418 <hbbrent@centurylink.net>
2017-05-04 03:12:49 -0400

    styling changes

--- public/resources/js/custom/allpages.js
+++ public/resources/js/custom/allpages.js
@@ -1,12 +1,24 @@
 //Prompt confirmation of logout
 function confirmLogout() {
-  if (confirm("Are you sure you want to logout?") == true) {
-    firebase.auth().signOut();
-    window.location.href = "index.html";
-  }
-  else{
-      return;
-  }
+    swal({
+            title: "Are you sure you want to logout?",
+            text: "",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#0288d1",
+            confirmButtonText: "Yes",
+            cancelButtonText: "No",
+            closeOnConfirm: false,
+            closeOnCancel: true
+        },
+        function (isConfirm) {
+            if (isConfirm) {
+                firebase.auth().signOut();
+                window.location.href = "index.html";
+            } else {
+                return;
+            }
+        });
 }
 
 //Initialize Firebase
@@ -28,5 +40,5 @@ function initDateField(date_field_id) {
 
 //Redirect to choose_form.html
 function backToChoose() {
-  window.location.href = "choose_form.html";
+    window.location.href = "choose_form.html";
 }
--- public/resources/js/custom/signin.js
+++ public/resources/js/custom/signin.js
@@ -8,39 +8,33 @@ $(document).ready(function () {
  * Handles the sign in button press.
  */
 function toggleSignIn() {
-    if (firebase.auth().currentUser) {
-        // [START signout]
-        firebase.auth().signOut();
-        console.log("signed out");
-        // [END signout]
-    } else {
+    if (!firebase.auth().currentUser) {
         var email = document.getElementById('email').value + "@bngc.com";
         var password = document.getElementById('password').value;
-        if (email.length < 4) {
-            alert('Please enter an email address.');
+        if (email.length < 10) {
+            sweetAlert("Login Issue", 'Please enter a valid username');
             return;
         }
         if (password.length < 4) {
-            alert('Please enter a password.');
+            sweetAlert("Login Issue", "Invalid password");
             return;
         }
-        // Sign in with email and pass.
-        // [START authwithemail]
+
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            // [START_EXCLUDE]
             if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
+                sweetAlert("Login Issue",'Invalid password');
             } else {
-                alert(errorMessage);
+                if (errorMessage == "There is no user record corresponding to this identifier. The user may have been deleted."){
+                    sweetAlert("Login Issue", "Unknown username");
+                } else {
+                    sweetAlert("Login Issue", errorMessage);
+                }
             }
             console.log(error);
             document.getElementById('quickstart-sign-in').disabled = false;
-            // [END_EXCLUDE]
         });
-        // [END authwithemail]
     }
     document.getElementById('quickstart-sign-in').disabled = true;
 }
@@ -54,45 +48,17 @@ function toggleSignIn() {
  */
 function initApp() {
     // Listening for auth state changes.
-    // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function (user) {
-        // [START_EXCLUDE silent]
-        //  document.getElementById('quickstart-verify-email').disabled = true;
-        // [END_EXCLUDE]
         if (user) {
             // User is signed in.
             var displayName = user.displayName;
             var email = user.email;
             var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
             var uid = user.uid;
-            var providerData = user.providerData;
             window.location.href = "choose_form.html";
-            // [START_EXCLUDE silent]
-            //    document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-            //    document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-            /*  if (!emailVerified) {
-      document.getElementById('quickstart-verify-email').disabled = false;
-    }*/
-            // [END_EXCLUDE]
         } else {
-            // User is signed out.
-            // [START_EXCLUDE silent]
-            //  document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
             document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-            //  document.getElementById('quickstart-account-details').textContent = 'null';
-            // [END_EXCLUDE]
         }
-        // [START_EXCLUDE silent]
-        //    document.getElementById('quickstart-sign-in').disabled = false;
-        // [END_EXCLUDE]
     });
-    // [END authstatelistener]
-
     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-    //document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-    //  document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-    //document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
 }
