770f4da58207addac47a0df0f7733dbfdeaeb03f
jyoon2 <jyoon2@nd.edu>
2017-05-04 21:31:36 -0400

    logout button on confirmation page works. changed text/placeholder for name of member to member id

--- public/resources/js/custom/confirmation.js
+++ public/resources/js/custom/confirmation.js
@@ -1,4 +1,6 @@
-var doc_def = sessionStorage.getItem('doc_def');
-doc_def = JSON.parse(doc_def)
-pdfMake.createPdf(doc_def).open();
-sessionStorage.clear();
+$(document).ready(function () {
+    var doc_def = sessionStorage.getItem('doc_def');
+    doc_def = JSON.parse(doc_def)
+    pdfMake.createPdf(doc_def).open();
+    sessionStorage.clear();
+});
