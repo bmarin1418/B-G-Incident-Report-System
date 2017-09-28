ae89dc425eb8dc5d3be6adfba66eeea2490882d9
bmarin1418 <hbbrent@centurylink.net>
2017-09-28 11:12:43 -0400

    Data pulling from db works

--- public/resources/js/custom/new_page.js
+++ public/resources/js/custom/new_page.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     firebaseInit();
-    var userId = firebase.auth().currentUser.uid;
+    //var userId = firebase.auth().currentUser.uid;
     firebase.database().ref('/locations/carmichael/').once('value').then(function(snapshot) {
       console.log("Value is ");
       console.log(snapshot.val());
