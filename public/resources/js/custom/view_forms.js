d01600bf956df63dbb856453a407153ea47689a0
Taylor Rongaus <trongaus@taylors-mbp-2.dhcp.nd.edu>
2017-11-09 15:18:50 -0500

    printing json of forms to the page wahoo

--- public/resources/js/custom/view_forms.js
+++ public/resources/js/custom/view_forms.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
     firebaseInit();
+    // get the url parameters and parse them
+    var urlToParse = location.search;
+    var params = parseQueryString(urlToParse);
+    var forms = params.forms_found.split("%20");
+    for (i=0; i<forms.length; i++) {
+      if (forms[i] == "and") {
+        forms.splice(i, 1);
+      }
+    }
+    // console.log(params);
+    // console.log(forms);
+    // obtain the different
+    var og_str = '/locations/carmichael/students/' + params.uid;
+    for (i=0; i<forms.length; i++) {
+      var new_str = og_str + '/' + forms[i].toLowerCase();
+      firebase.database().ref(new_str).once('value').then(function(snapshot) {
+        console.log(snapshot.val());
+        document.getElementById("json").innerHTML += JSON.stringify(snapshot.val(), undefined, 2) + "\n\n\n";
+      });
+    }
 
-    firebase.database().ref('/locations/carmichael/').once('value').then(function(snapshot) {
-      console.log(snapshot.val());
-    });
 });
 
 
