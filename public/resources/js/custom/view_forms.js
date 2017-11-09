eeae991a6d404f99da4df25ed9553fbdb26b21bd
Taylor Rongaus <trongaus@taylors-mbp-2.attlocal.net>
2017-11-08 19:13:00 -0500

    successfully retreiving url parameter from view forms page

--- public/resources/js/custom/view_forms.js
+++ public/resources/js/custom/view_forms.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     firebaseInit();
 
-    firebase.database().ref(str).once('value').then(function(snapshot) {
-      console.log(snapshot)
-    }
+  //  firebase.database().ref(str).once('value').then(function(snapshot) {
+  //    console.log(snapshot);
+  //  });
 
     //firebase.database().ref('/locations/carmichael/').once('value').then(function(snapshot) {
     //  console.log(snapshot.val());
     //});
 });
+
+var parseQueryString = function(url) {
+  var urlParams = {};
+  url.replace(
+    new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+    function($0, $1, $2, $3) {
+      urlParams[$1] = $3;
+    }
+  );
+  return urlParams;
+}
