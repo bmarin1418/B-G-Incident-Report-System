d945ab29e78274f0e4e79a3adeafcc60cc9ada74
Taylor Rongaus <trongaus@taylors-mbp-2.dhcp.nd.edu>
2018-02-25 20:56:56 -0500

    added functionality for form query with specified location

--- public/resources/js/custom/metrics.js
+++ public/resources/js/custom/metrics.js
@@ -5,80 +5,100 @@ $(document).ready(function () {
     //});
 });
 
+// Prompt errors w/ searching by uid
+function uidError(message) {
+    swal({
+            title: "Error",
+            text: message,
+            type: "warning",
+            showCancelButton: true,
+            showConfirmButton: false,
+            cancelButtonText: "Close",
+            closeOnCancel: true
+        });
+}
+
+function loadingForms() {
+    swal({
+        title: "Searching...",
+        text: "Please wait",
+        imageUrl: "https://github.com/bmarin1418/B-G-Incident-Report-System/blob/59db4d0ad1ec3d157b975763245c93303f6ae1b6/public/resources/img/ajax-loader.gif?raw=true",
+        showConfirmButton: false,
+        allowOutsideClick: false
+    });
+}
+
+// Search by student UID
 function searchUID() {
     var uid = document.getElementById('userID').value;
+    var locationSelected = false;
     if (uid === "") {
-        document.getElementById("search_result").innerHTML = 'Please enter a student ID'
+        // throw error if no ID number was entered
+        uidError("Please enter a student ID number")
     } else {
         console.log(uid);
-        var str = '/locations/carmichael/students/';
-        str += uid;
-        console.log(str)
-        document.getElementById("showForms").style = "display: none;";
-        document.getElementById("forms_found").innerHTML = '';
-        firebase.database().ref(str).once('value').then(function (snapshot) {
-            // make sure the student being searched for actually exists
-            if (snapshot.val() != null) {
-                var phrase = 'Student ';
-                phrase += uid;
-                phrase += ' found. Incident reports include: ';
-                // determine which incident reports exist in the database
-                document.getElementById("search_result").innerHTML = phrase;
-                var behavior = str + '/behavior';
-                var accident = str + '/accident';
-                var general = str + '/general';
-                console.log(behavior)
+        // determine the location that we're looking at via the locations dropdown
+        if (document.getElementById("location").value != "") {
+            locationSelected = true;
+            var str = '/locations/' + document.getElementById("location").value + '/students/';
+        }
+        else {
+            uidError("Please select a location");
+        }
+        // only proceed with the search for uid if they have selected a location
+        if (locationSelected == true) {
+            str += uid;
+            console.log(str)
+            document.getElementById("showForms").style = "display: none;";
+            document.getElementById("forms_found").innerHTML = '';
+            firebase.database().ref(str).once('value').then(function (snapshot) {
+                // make sure the student being searched for actually exists
+                if (snapshot.val() != null) {
+                    var behavior = str + '/behavior';
+                    var accident = str + '/accident';
+                    var general = str + '/general';
+                    loadingForms();
                     // check for behavior forms
-                var form_types = ''
-                firebase.database().ref(behavior).once('value').then(function (snapshot) {
-                    if (snapshot.val() != null) {
-                        if (form_types == '') {
-                            form_types = 'Behavior';
-                        } else {
-                            form_types += ' and Behavior';
+                    var form_types = ''
+                    firebase.database().ref(behavior).once('value').then(function (snapshot) {
+                        if (snapshot.val() != null) {
+                            if (form_types == '') {
+                                form_types = 'Behavior';
+                            } else {
+                                form_types += ' and Behavior';
+                            }
                         }
-                        document.getElementById("forms_found").innerHTML = form_types;
-                        console.log('behavior found')
-                    }
-                });
-                // check for accident forms
-                firebase.database().ref(accident).once('value').then(function (snapshot) {
-                    if (snapshot.val() != null) {
-                        if (form_types == '') {
-                            form_types = 'Accident';
-                        } else {
-                            form_types += ' and Accident';
+                    });
+                    // check for accident forms
+                    firebase.database().ref(accident).once('value').then(function (snapshot) {
+                        if (snapshot.val() != null) {
+                            if (form_types == '') {
+                                form_types = 'Accident';
+                            } else {
+                                form_types += ' and Accident';
+                            }
                         }
-                        document.getElementById("forms_found").innerHTML = form_types;
-                        console.log('accident found')
-                    }
-                });
-                // check for general incident forms
-                firebase.database().ref(general).once('value').then(function (snapshot) {
-                    if (snapshot.val() != null) {
-                        if (form_types == '') {
-                            form_types = 'General Incident';
-                        } else {
-                            form_types += ' and General Incident';
+                    });
+                    // check for general incident forms
+                    firebase.database().ref(general).once('value').then(function (snapshot) {
+                        if (snapshot.val() != null) {
+                            if (form_types == '') {
+                                form_types = 'General Incident';
+                            } else {
+                                form_types += ' and General Incident';
+                            }
                         }
-                        document.getElementById("forms_found").innerHTML = form_types;
-                        console.log('general found')
-                    }
-                });
-                document.getElementById("showForms").style = "display: inline-block;";
-            } else {
-                // print error message if not
-                document.getElementById("search_result").innerHTML = 'You have either entered an incorrect student ID number, or that student has no incident reports in the database. Please try again.'
-            }
-
+                    });
+                    // wait three seconds for query to complete
+                    setTimeout(function(){
+                        window.location.href = 'view_forms.html'+'?uid='+document.getElementById('userID').value+'&forms_found='+form_types
+                    }, 3000);
+                } else {
+                    // print error message if not
+                    uidError("You have either entered an incorrect student ID number, or that student has no incident reports in the database. Please try again.")
+                }
         });
+        }
     }
 }
 
-
-// query the entire JSON
-// what are the keys?
-// go through all of the keys
-// or
-// access just the json w/ those keys
-// query on those keys
