9ddd2dbc5757ff7bf8009fc6434496b7f501cfc6
Sarah Olson <olsons@users.noreply.github.com>
2018-04-17 15:45:38 -0400

    Update mem_behavior.js

--- public/resources/js/custom/mem_behavior.js
+++ public/resources/js/custom/mem_behavior.js
@@ -191,6 +191,12 @@ function submitClickHandler(inputValidator) {
 
 // Send the form info to the database and then create the JSON for a pdf
 function submitAndPrint(newForm, club) {
+    if (noBoxChecked(behaviors){
+            sweetAlert("Form Did Not Submit", "Please check at least one box under the \"Inappropriate Behavior Type\");
+        }
+    if (noBoxChecked(consequences)) {
+            sweetAlert("Form Did Not Submit", "Please check at least one box under the \"Consequences\" section");
+      
     firebase.database().ref('locations/' + club + '/students/' + newForm["memberId"] + '/behavior/').push(newForm, function (err) {
         if (err) {
             sweetAlert("Form Did Not Submit", "Check your internet connection and try again");
